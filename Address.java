36f7a43f8e8dbaf4e7187a9d1262ea85b3bea004
Akshaya Venky <akshaya-venkatesh8@users.noreply.github.com>
2016-06-08 10:47:13 +0530

    Update Address.java

--- Address.java
+++ Address.java
@@ -1,21 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.github.akshayavenkatesh8;
-
-import org.w3c.dom.Node;
-
-/**
- *
- * @author AKSHAYA
- */
 public class Address {
-
-    /**
-     *
-     */
     public String name;
     public String streetNumber;
     public String streetName;
@@ -23,15 +7,9 @@ public class Address {
     public String city;
     public String stateCode;
     public String zipCode;
-    
-   
-
-      public String toString()
+    public String toString()
     {
        return "Address :\n"+name+",\n"+streetNumber+" "+streetName+",\n"+secondaryAddress+",\n"+city+",\n"+stateCode+" "+zipCode;
-       
-        
-                
     }
-    
-}
+}    
+
